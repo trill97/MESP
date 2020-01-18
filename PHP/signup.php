a2bc88b97788631af48aebd63364be951b269f6c
micha777 <59413428+micha777@users.noreply.github.com>
2020-01-18 19:35:24 +0530

    Create signup.php

---
+++ PHP/signup.php
@@ -0,0 +1,34 @@
+<?php
+
+$uname=$_POST['uname'];
+$mail=$_POST['mail'];
+$pw=$_POST['pw'];
+
+$dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "signup";
+
+    $conn = new mysqli($dbServername,$dbUsername,$dbPassword,$dbName); //connection
+
+    if(mysqli_connect_error()){
+        die('Connect Error('.mysqli_connect_error().')'
+        .mysqli_connect_error());
+    }
+    else
+    {
+        $sql = "INSERT INTO sign (userName,Email,password)
+            values('$uname','$mail','$pw') ";
+        
+     
+      if($conn->query($sql)){
+        header('Location: index.html');
+        
+      }
+      else{
+          echo "Error:" .$sql."<br>".$conn->error;
+      }
+     $conn->close();
+    }
+
+?>
