b2f7bdd118d9cff326bb964c95471c0a21e89511
micha777 <59413428+micha777@users.noreply.github.com>
2020-01-19 16:02:10 +0530

    Update action_page.php

--- PHP/action_page.php
+++ PHP/action_page.php
@@ -15,3 +15,25 @@
     $dbPassword = "";
     $dbName = "contactus";
 
+ $conn = new mysqli($dbServername,$dbUsername,$dbPassword,$dbName); 
+
+    if(mysqli_connect_error()){
+        die('Connect Error('.mysqli_connect_error().')'
+        .mysqli_connect_error());
+    }
+    else
+    {
+        $sql = "INSERT INTO contact (Fullname,Email,Phone,Message)
+            values('$uname','$email','$tel','$msg') ";
+        
+     
+      if($conn->query($sql)){
+       header('Location: ../index.html');
+        
+      }
+      else{
+          echo "Error:" .$sql."<br>".$conn->error;
+      }
+     $conn->close();
+    }
+?>
